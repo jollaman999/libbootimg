c1253afb8f8175dcbcba2778e552d6b2a5d8bb75
zhuowei <zhuoweizhang@yahoo.com>
2017-09-23 00:12:07 -0400

    Fix a memset call in libbootimg_init_new
    The original memset call has the arguments reversed. It doesn't seem to affect anything since the header is set later anyways, but GCC catches this as a warning.

--- src/libbootimg.c
+++ src/libbootimg.c
@@ -179,7 +179,7 @@ void libbootimg_init_new(struct bootimg *img)
     memcpy(img->hdr.magic, BOOT_MAGIC, BOOT_MAGIC_SIZE);
     img->hdr.page_size = DEFAULT_PAGE_SIZE;
     img->hdr_info = malloc(sizeof(struct boot_img_elf_info));
-    memset(img->hdr_info, sizeof(struct boot_img_elf_info), 0);
+    memset(img->hdr_info, 0, sizeof(struct boot_img_elf_info));
     img->hdr_info->cmdline_signature = 0;
     img->is_elf = 0;
 
